c85a6d638834f4631622d5e6777e39ae07879cf9
dessant <dessant@kivy.org>
2015-01-07 02:10:54 +0200

    remove dev assets from build directory

--- bextr/freeze.py
+++ bextr/freeze.py
@@ -1,9 +1,10 @@
-from os import environ
+from os import environ, remove
 environ['FREEZING_SITE'] = '1'
-from os.path import exists
+from os.path import join, isdir, isfile
 from shutil import rmtree
 from flask_frozen import Freezer
 from app import create_app
+from app.assets import assets
 
 
 app = create_app('config')
@@ -11,9 +12,17 @@ app = create_app('config')
 freezer = Freezer(app)
 
 build_dir = app.config['FREEZER_DESTINATION']
-if exists(build_dir):
+if isdir(build_dir):
     rmtree(build_dir)
 
 freezer.freeze()
-# serve the frozen site for testing at http://127.0.0.1:5000
+
+# Delete development assets from the build directory.
+for k, v in assets._named_bundles.items():
+    for asset in v.contents:
+        asset_path = join(build_dir, 'static', asset)
+        if isfile(asset_path):
+            remove(asset_path)
+
+# Serve the frozen site for testing at http://127.0.0.1:5000.
 # freezer.serve()
